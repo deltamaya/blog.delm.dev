bd39bdd95edae905fd09a715e8eb85da921a478b
deltamaya <deltamaya553@gmail.com>
2024-12-18 21:35:21 +0800

    allow quote in heading

--- src/lib/heading.ts
+++ src/lib/heading.ts
@@ -3,7 +3,7 @@ import { marked, type Tokens } from 'marked';
 
 
 export function headingHandler(heading:Tokens.Heading,headings: { depth: number; text: string; id: string }[]){
- const id = heading.text.toLowerCase().replace(/\s+/g, '-');
+ const id = heading.text.toLowerCase().replace(/\s+/g, '-').replace('"','');
 	headings.push({ depth:heading.depth,text: heading.text, id})
 	return  `<h${heading.depth} id="${id}">${marked.Parser.parseInline(heading.tokens)}</h${heading.depth}>`;
 }
