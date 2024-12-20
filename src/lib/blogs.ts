1b6cda1a5db618b2823ad35824fbfd9f8ec60e6c
deltamaya <deltamaya553@gmail.com>
2024-12-20 14:15:34 +0800

    update

--- src/lib/blogs.ts
+++ src/lib/blogs.ts
@@ -10,43 +10,52 @@ export interface BlogMeta {
 	tags: string[]; // 博客包含的标签
 }
 
+const supportedLanguages=['en','zh-cn','zh-tw']
+
 // 定义全局数据存储
-let blogs: BlogMeta[] = []; // 存储所有博客
-const tagToBlogs: Map<string, BlogMeta[]> = new Map(); // tag -> blogs 的映射
+
+const blogs = new Map<string,BlogMeta[]>(); // 存储所有博客
+const tagToBlogs = new  Map<string,Map<string, BlogMeta[]>>; // tag -> blogs 的映射
 
 function loadAllBlogs() {
 	console.log('loading all posts');
-	const postsDirectory = path.resolve(`content/${languageTag()}`);
-	const files = fs.readdirSync(postsDirectory);
-
-	blogs = files.map((file) => {
-		const filePath = path.join(postsDirectory, file);
-		const content = fs.readFileSync(filePath, 'utf-8');
-		const { data } = matter(content);
-		return {
-			slug: file.replace(/\.md$/, ''),
-			date: data.date,
-			title: data.title,
-			tags: data.tags
-		};
-	});
-	blogs.sort((a, b) => b.date.getTime() - a.date.getTime());
-	blogs.forEach((blog) => {
-		blog.tags.forEach((tag) => {
-			if (!tagToBlogs.has(tag)) {
-				tagToBlogs.set(tag, []);
-			}
-			tagToBlogs.get(tag)!.push(blog);
+	for (const lang of supportedLanguages){
+		console.log(lang)
+			const postsDirectory = path.resolve(`content/${lang}`);
+		const files = fs.readdirSync(postsDirectory);
+		const temp = files.map((file) => {
+			const filePath = path.join(postsDirectory, file);
+			const content = fs.readFileSync(filePath, 'utf-8');
+			const { data } = matter(content);
+			return {
+				slug: file.replace(/\.md$/, ''),
+				date: data.date,
+				title: data.title,
+				tags: data.tags
+			};
 		});
-	});
+		temp.sort((a, b) => b.date.getTime() - a.date.getTime());
+		temp.forEach((blog) => {
+			blog.tags.forEach((tag) => {
+				if(!tagToBlogs.has(lang)){
+					tagToBlogs.set(lang,new Map<string,BlogMeta[]>)
+				}
+				const langTaggedBlogs=tagToBlogs.get(lang)!
+				if (!langTaggedBlogs!.has(tag)) {
+					langTaggedBlogs.set(tag, []);
+				}
+				langTaggedBlogs.get(tag)!.push(blog);
+			});
+		});
+		blogs.set(lang,temp)
+	}
 }
 
 loadAllBlogs();
-
 export function getAllBlogsMeta() {
-	return blogs;
+	return blogs.get(languageTag());
 }
 
 export function getTaggedBlogsMeta(tag: string) {
-	return tagToBlogs.get(tag);
+	return tagToBlogs.get(languageTag())!.get(tag);
 }
