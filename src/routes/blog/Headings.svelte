6dfd3be1a11636387da5266d9341e4ba13d54c00
deltamaya <deltamaya553@gmail.com>
2024-12-30 23:18:13 +0800

    Blog Headings: Make long titles wrap

--- src/routes/blog/Headings.svelte
+++ src/routes/blog/Headings.svelte
@@ -6,14 +6,13 @@
 <nav
 	class="my-5 w-full border-l-2
  border-neutral-200 px-4"
-	style="scrollbar-width: none;-ms-overflow-style: none"
 >
 	<h2 class="mb-1 text-lg font-bold">{m.TableOfContents()}</h2>
 	<div class="flex flex-col flex-wrap">
 		{#each headings as heading}
 			<a
 				href={`#${heading.id}`}
-				class="w-full truncate text-neutral-900 hover:text-red-600 hover:underline"
+				class="w-full text-neutral-900 hover:text-red-600 hover:underline flex flex-wrap"
 				style={`margin-left: ${(heading.depth - 1) * 12}px;font-weight: ${(7 - heading.depth) * 100}`}
 			>
 				{heading.text}
@@ -23,7 +22,5 @@
 </nav>
 
 <style>
-	::-webkit-scrollbar {
-		display: none;
-	}
+
 </style>
