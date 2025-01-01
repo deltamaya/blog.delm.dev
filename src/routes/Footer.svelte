64cb6c8b056fbecad3a44ed14582efa3f09a0706
deltamaya <me@delm.dev>
2025-01-01 08:34:58 +0800

    update copyleft time

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -4,7 +4,7 @@
 	<div class="z-40 flex flex-wrap items-center justify-center text-center text-sm">
 		<span class="inline-block">©&nbsp;</span>
 		<span
-			>2024 <a href="https://www.delm.dev" class="hover:underline">Maya</a>. All works
+			>{new Date().getFullYear()} <a href="https://www.delm.dev" class="hover:underline">Maya</a>. All works
 			<a href="https://www.gnu.org/copyleft/" class="hover:underline">Copyleft</a>.</span
 		>
 	</div>
