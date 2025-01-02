f1f6c7ff5946f68737c60d4871385f3f9ad8ec6a
deltamaya <me@delm.dev>
2025-01-02 11:52:57 +0800

    add 404 not found page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<div class="w-full flex flex-col justify-center items-center space-y-5 min-h-[calc(100vh-6.5rem)]">
+	<div class="text-6xl font-bold">Oops!😢</div>
+	<div class="flex justify-center items-center">
+		<div class="text-5xl font-bold">404&nbsp;</div>
+		<div class="text-4xl font-bold bg-red-500 px-3 py-2 text-white">Not Found</div>
+	</div>
+</div>
