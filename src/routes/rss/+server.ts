8fb22ee014ffad7e901dc2056fd0c4c6c9ff2dc1
deltamaya <me@delm.dev>
2025-04-26 09:19:37 +0800

    update

--- src/routes/rss/+server.ts
+++ src/routes/rss/+server.ts
@@ -1,22 +1,23 @@
 import { languageTag } from '$lib/paraglide/runtime';
 import { getAllBlogsMeta } from '$lib/blogs';
 
-const blogs=getAllBlogsMeta().slice(0,20)
+const blogs=getAllBlogsMeta().slice(0,10)
 
 
 function generateRSS(blogs: any) {
 	return `<?xml version="1.0" encoding="UTF-8"?>
 	 <rss version="2.0">
       <channel>
-        <title>DELM Blog</title>
+        <title>DELM blog</title>
         <link>https://blog.delm.dev</link>
-        <description>deltamaya blog project</description>
+        <description>deltamaya blog project, shares my insights on programming, technology and life</description>
         ${blogs.map((blog)=>{
 			return `
 	<item>
 		<title>${blog.title}</title>
 		<link>https://blog.delm.dev${languageTag()==='en'?'':'/'+languageTag()}/blog/${blog.slug}</link>
 		<description>${blog.title}</description>
+		<pubDate>${blog.date}</pubDate>
 	</item>
 			`
 				}
