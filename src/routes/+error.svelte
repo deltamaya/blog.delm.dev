d6eaf526ac7fa76bcc00c9457bf86a26e2b24147
deltamaya <me@delm.dev>
2025-05-11 20:03:13 +0800

    fix error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,8 +4,9 @@
 </script>
 <div class="w-full flex flex-col justify-center items-center space-y-5 min-h-[calc(100vh-124px)]">
 	<div class="text-6xl font-bold">Oops!😢</div>
-	<div class="flex justify-center items-center">
-		<div class="text-5xl font-bold">404&nbsp;</div>
-		<div class="text-4xl font-bold bg-red-500 px-3 py-2 text-white">Not Found</div>
+	<div class="flex justify-center items-center text-5xl font-bold">
+		404&nbsp;
+		<span class="text-red-600">N</span>ot&nbsp;
+		<span class="text-red-600">F</span>ound
 	</div>
 </div>
