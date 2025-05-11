2b94bed68bef71ee15535d1924ba2ba8b59e12ae
deltamaya <me@delm.dev>
2025-05-11 22:05:14 +0800

    update footer

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -4,13 +4,13 @@
 <footer
 	class="font-inter z-40 flex h-10 items-center justify-center bg-neutral-50 py-4 font-Sans text-neutral-600 dark:bg-neutral-900 dark:text-neutral-50 transition-colors"
 >
-	<div class="z-40 flex flex-wrap items-center justify-center text-center text-sm">
+	<div class="z-40 flex flex-wrap items-center justify-center text-center sm:text-sm text-xs">
 		<span class="inline-block">©&nbsp;</span>
-		<span>{new Date().getFullYear()} <a href="https://www.delm.dev" class="hover:underline">deltamaya</a>.
+		<span>{new Date().getFullYear()} <a href="https://www.delm.dev" class="underline">deltamaya</a>.
 			{#if languageTag()==='en'}
-				All works licensed under <a href="https://creativecommons.org/licenses/by/4.0/" class="hover:underline">CC BY 4.0</a>.
+				All works licensed under <a href="https://creativecommons.org/licenses/by/4.0/" class="underline">CC BY 4.0</a>.
 			{:else}
-				所有作品均在<a href="https://creativecommons.org/licenses/by/4.0/" class="hover:underline">CC BY 4.0</a>许可下发布.
+				所有作品均在<a href="https://creativecommons.org/licenses/by/4.0/" class="underline">CC BY 4.0</a>许可下发布.
 			{/if}
 		</span>
 	</div>
