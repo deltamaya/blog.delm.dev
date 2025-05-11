e3e8e992268b84fa3984da212ad0ae2b9c150810
deltamaya <me@delm.dev>
2025-05-11 20:17:08 +0800

    fix search page

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import * as m from '$lib/paraglide/messages.js';
+	import Icon from '@iconify/svelte';
 	let keyword = $state('');
 
 	function handleSearch() {
@@ -12,24 +13,25 @@
 <svelte:head>
 	<title>{m.Search()} - DELM</title>
 </svelte:head>
-<div class="flex h-full w-full flex-col items-center">
+<div class="flex h-full w-full flex-col items-center transition-colors">
 	<div class="h-full w-full max-w-[48rem] p-4">
 		<div class="py-6 text-4xl font-bold">
 			{m.Search()}
 		</div>
-		<div class="flex flex-col items-center justify-center">
+		<div class="flex items-center justify-center">
 			<input
 				type="text"
 				bind:value={keyword}
 				placeholder={m.SearchHere()}
 				onkeydown={(e) => e.key === 'Enter' && handleSearch()}
-				class="w-full rounded-sm border-2 border-neutral-200 px-3 py-2 font-bold dark:bg-neutral-800 text-neutral-900 dark:text-white dark:placeholder-neutral-100 "
+				class="w-full border-2 border-neutral-200 dark:border-neutral-800 px-3 py-2 font-bold dark:bg-neutral-900 text-neutral-900 dark:text-white dark:placeholder-neutral-200 "
 			/>
 			<button
+				aria-label="search"
 				onclick={handleSearch}
-				class="mt-3 flex w-32 items-center justify-center rounded-lg bg-red-500 px-3 py-2 font-bold text-white"
+				class="flex items-center justify-center px-3 py-2 font-bold hover:text-red-600 transition-colors"
 			>
-				{m.Search()}
+				<Icon icon="material-symbols:search" width="24" height="24" />
 			</button>
 		</div>
 	</div>
