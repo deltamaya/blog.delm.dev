65b7322c0287022ea0f3f1b5ca1073f6d489dded
deltamaya <me@delm.dev>
2025-12-24 22:40:28 +0800

    fix

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -24,15 +24,15 @@ export async function load({ params }) {
 		const highlighted = validLang ? hljs.highlight(text, { language: lang }).value : text;
 		return `<div class="relative group">
 				<pre><code class="hljs ${lang ? `language-${lang}` : ''} !bg-neutral-900">${highlighted}</code></pre>
-				<button class="copy-button opacity-0 absolute group-hover:opacity-100 top-2 right-2 bg-neutral-900 border-[1px] border-neutral-700 dark:border-neutral-800 text-white text-sm px-2 py-1 transition-opacity duration-200">Copy</button>
+				<button class="copy-button opacity-0 absolute group-hover:opacity-100 top-2 right-2 bg-neutral-900 border-[1px] border-neutral-700 dark:border-neutral-800 text-white text-sm px-2 py-1 transition-opacity duration-200 font-Sans">Copy</button>
 				</div>`;
 	};
 	renderer.image = function ({ href, title, text }) {
-		let staticPath=''
-		if(href.startsWith('/')||href.startsWith('http://')||href.startsWith('https://')){
+		let staticPath = ''
+		if (href.startsWith('/') || href.startsWith('http://') || href.startsWith('https://')) {
 			staticPath = href
-		}else{
-			staticPath=`/${href}`
+		} else {
+			staticPath = `/${href}`
 		}
 		const titleAttr = title ? ` title="${title}"` : '';
 		return `<figure class="justify-center flex flex-col my-5"><img class="!my-0" src="${staticPath}" alt="${text}"${titleAttr} /><figcaption class="self-center text-sm dark:text-neutral-400 text-neutral-600 !mt-2">${text}</figcaption></figure>`;
